c8d7702b2af1cd131ec014ff7167c51f83a1ff39
boehmec <dev@boehme.me>
2024-11-20 15:08:10 +0100

    use voltage

--- src/main/osd/osd.c
+++ src/main/osd/osd.c
@@ -456,7 +456,8 @@ void pgResetFn_osdElementConfig(osdElementConfig_t *osdElementConfig)
     osdElementConfig->item_pos[OSD_WARNINGS] = OSD_POS((midCol - 6), (midRow + 3)) | profileFlags;
 
     //ADD OUR RACE GATE TIMER FIXED POS
-    osdElementConfig->item_pos[OSD_RACE_GATE_TOTAL_LAPS] = OSD_POS((midCol - 1), (midRow + 1)) | OSD_PROFILE_1_FLAG;
+    // osdElementConfig->item_pos[OSD_RACE_GATE_TOTAL_LAPS] = OSD_POS((midCol - 1), (midRow + 1)) | OSD_PROFILE_1_FLAG;
+    osdElementConfig->item_pos[OSD_AVG_CELL_VOLTAGE] = OSD_POS((midCol - 1), (midRow + 1)) | OSD_PROFILE_1_FLAG;
 
     // Default to old fixed positions for these elements
     osdElementConfig->item_pos[OSD_CROSSHAIRS]         = OSD_POS((midCol - 2), (midRow - 1));
